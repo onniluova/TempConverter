b363bd729a0cc2c9ecedc5e2ffd7d0cc2ffafc21
onboni <onni.luova@gmail.com>
2024-09-18 13:21:33 +0300

    tempconverter

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    private TemperatureConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    public void testKelvinToCel() {
+        assertEquals(-273.15, converter.kelvinToCel(0), 0.01);
+        assertEquals(0, converter.kelvinToCel(273.15F), 0.01);
+        assertEquals(26.85, converter.kelvinToCel(300), 0.01);
+    }
+}
